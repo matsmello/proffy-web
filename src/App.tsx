8bf5cf8233b4b86644d858e269bb0fa3bb7c2425
Matheus Mello <matheusmello_22@hotmail.com>
2020-08-07 11:45:05 -0300

    Adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './styles/global.css'
-import Landing from './pages/Landing'
+import Routes from './routes';
 
 function App() {
-  return (
-    <Landing />
-  );
+  return <Routes />
 }
 
 export default App;
